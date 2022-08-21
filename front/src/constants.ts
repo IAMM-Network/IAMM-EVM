0bfb42454ee68129b8c5fbf567a57d66b606cd0f
luisantoniocrag <tricubeart@gmail.com>
2022-08-21 08:36:03 -0500

    constans

---
+++ front/src/constants.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    CONTRACT_ISSUER: "0xa3681AE14FcAF9e11C826448ABe29b03f5b029f4",
+    CONTRACT_CLAIM_VERIFIER: "0xe918d472A67507fF570ff505BBE8520FA85f0D1C",
+    PROFILES_TYPES: {
+        "creator": 5,
+        "builder": 6,
+        "curator": 7,
+        "collector": 8 //Default profile type when user connect the wallet for the first time
+    },
+    ALCHEMY_URI: 'https://polygon-mumbai.g.alchemy.com/v2/aQXtDzeAsXSZm6A-BYHZL_BMcFUvxCE5'
+}
